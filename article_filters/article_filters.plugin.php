5a5a015ca689e3d25bcd4ef6c507003bc7170525
Kaspars Dambis <kaspars@konstruktors.com>
2013-01-22 21:13:39 +0200

    Add comments

--- article_filters/article_filters.plugin.php
+++ article_filters/article_filters.plugin.php
@@ -3,17 +3,21 @@
 class article_filters extends orbiter {
 
 	function article_filters() {
+		// Hook in right before content object is passed to template for rendering
 		orbiter::add_filter( 'render_article_html', array( $this, 'filters' ), 5 );
 	}
 
 	function filters( $object, $request_uri ) {
+		// We are on home page, no need to run
 		if ( empty( $request_uri ) )
 			return $object;
 
+		// Add all child pages to children global
 		foreach ( $object['articles'] as $article )
 			if ( strstr( $article['uri'], $request_uri ) && ! empty( $article['slug'] ) )
 				$object['children'][] = $article;
 
+		// Add all blog posts to blog_posts global
 		foreach ( $object['articles'] as $article )
 			if ( strstr( $article['uri'], 'blog' ) && ! empty( $article['slug'] ) )
 				$object['blog_posts'][] = $article;
