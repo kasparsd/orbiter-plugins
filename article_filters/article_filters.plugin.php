7cd8d20a6db1269e624cece414d1fb529e018c24
Kaspars Dambis <hi@kaspars.net>
2014-09-04 20:38:52 +0300

    Fix for 2.0

--- article_filters/article_filters.plugin.php
+++ article_filters/article_filters.plugin.php
@@ -1,28 +1,38 @@
 <?php
 
-class article_filters extends orbiter {
+class article_filters extends orbiter_plugin {
 
 	function article_filters() {
+
 		// Hook in right before content object is passed to template for rendering
-		orbiter::add_filter( 'render_article_html', array( $this, 'filters' ), 5 );
+		orbiter::add_filter( 'render_article_html_args', array( $this, 'filters' ), 5 );
+
 	}
 
-	function filters( $object, $request_uri ) {
-		// We are on home page, no need to run
-		if ( empty( $request_uri ) )
-			return $object;
+	function filters( $args, $article ) {
 
-		// Add all child pages to children global
+		$articles = orbiter::instance()->index();
+		$request_uri = dirname( $article['uri'] );
+
+		$children = array();
+		$blog_posts = array();
+
+		foreach ( $articles as $article )
+			if ( strstr( $article['uri'], $request_uri ) && $article['slug'] !== 'index' )
+				$children[] = $article;
+
 		foreach ( $object['articles'] as $article )
-			if ( strstr( $article['uri'], $request_uri ) && ! empty( $article['slug'] ) )
-				$object['children'][] = $article;
+			if ( strstr( $article['uri'], 'blog/' ) && ! empty( $article['slug'] ) )
+				$blog_posts[] = $article;
 
-		// Add all blog posts to blog_posts global
-		foreach ( $object['articles'] as $article )
-			if ( strstr( $article['uri'], 'blog' ) && ! empty( $article['slug'] ) )
-				$object['blog_posts'][] = $article;
+		if ( ! empty( $children ) )
+			$args[ 'children' ] = $children;
 
-		return $object;
+		if ( ! empty( $blog_posts ) )
+			$args['blog_posts'] = $blog_posts;
+
+		return $args;
+
 	}
 
 }
